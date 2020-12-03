00de4ad765deb16810f0a8dd3c09bb982443e23f
CianTivnan <cian.tivnan@yahoo.ie>
2020-12-03 11:27:39 +0000

    task 5 complete

---
+++ Exercise4/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void tbxInput_GotFocus(object sender, RoutedEventArgs e)
+        {
+            tbxInput.Clear();
+        }
+
+        private void btnButton1_Click(object sender, RoutedEventArgs e)
+        {
+            //read number
+            string input = (tbxInput.Text).Trim();
+
+            //check if valid number
+            int number;
+            bool isNum = int.TryParse(input, out number);
+
+            //display appropriate message
+            if (isNum)
+            {
+                MessageBox.Show("Value entered is a valid number");
+                tbxInput.Background = Brushes.Green;
+            }
+            else
+            {
+                MessageBox.Show("Value entered is not a valid number");
+                tbxInput.Background = Brushes.Red;
+            }
+        }
+    }
+}
---
+++ Exercise5/MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Exercise5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        static int initialNo;
+        static int secondNo;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void btnStart_Click(object sender, RoutedEventArgs e)
+        {
+            Random rng = new Random();
+            initialNo = rng.Next(1, 21);
+            tblkInitialNo.Text = "Initial Number : " + initialNo.ToString();
+        }
+
+        private void btnGuess_Click(object sender, RoutedEventArgs e)
+        {
+            Random rng2 = new Random();
+            secondNo = rng2.Next(1, 21);
+            bool equal = false;
+            tblkSecond.Text = "Second Number : " + secondNo.ToString();
+
+            if (secondNo == initialNo)
+            {
+                tblkResult.Text = "Numbers are equal, draw!";
+                equal = true;
+            }
+
+            if(rdoBigger.IsChecked == true && equal == false)
+            {
+                if(secondNo > initialNo)
+                {
+                    tblkResult.Text = "You Win!";
+                }
+                else
+                {
+                    tblkResult.Text = "You Lose!";
+                }
+            }
+
+            if (rdoSmaller.IsChecked == true && equal == false)
+            {
+                if (secondNo < initialNo)
+                {
+                    tblkResult.Text = "You Win!";
+                }
+                else
+                {
+                    tblkResult.Text = "You Lose!";
+                }
+            }
+        }
+    }
+}
